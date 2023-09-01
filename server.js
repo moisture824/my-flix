97556e1b453a8712d9dd2e6c25122cc7238e5234
moisture824 <kevinjmoy@gmail.com>
2023-09-01 10:45:47 -0400

    Define some missing variables

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
-const server = http.createServer((request, response) => {
-  const parsedUrl = url.parse(req.url, true);
+const server = http.createServer((request, responseponse) => {
+  const parsedUrl = url.parse(request.url, true);
   let filePath = 'index.html';
   if (parsedUrl.pathname.includes('documentation')) {
     filePath = 'documentation.html';
@@ -12,13 +12,13 @@ const server = http.createServer((request, response) => {
   }
   fs.readFile(filePath, (err, data) => {
     if (err) {
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('File not found\n');
+      response.statusCode = 404;
+      response.setHeader('Content-Type', 'text/plain');
+      response.end('File not found\n');
     } else {
-      res.statusCode = 200;
-      res.setHeader ('Content-Type', 'text/html');
-      res.end(data);  
+      response.statusCode = 200;
+      response.setHeader ('Content-Type', 'text/html');
+      response.end(data);  
     }
   });
 });
